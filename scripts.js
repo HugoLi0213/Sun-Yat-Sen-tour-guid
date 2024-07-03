224039d7ae29b533d15113efe66a4dff23341786
Hugoli0213 <s12332146@gmail.com>
2024-07-03 11:07:15 +0800

    add text func

--- scripts.js
+++ scripts.js
@@ -15,3 +15,12 @@ function playAudio(id) {
         audio.pause();
     }
 }
+
+function toggleText(id) {
+    var text = document.getElementById(id);
+    if (text.style.display === "none" || text.style.display === "") {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
